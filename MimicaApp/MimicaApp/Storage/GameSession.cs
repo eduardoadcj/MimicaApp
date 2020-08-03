9f825f3f71f291656a2cd315378c02f9f8e0a36d
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-08-03 15:30:06 -0300

    Criação de classe de armazenamento

---
+++ MimicaApp/MimicaApp/Storage/GameSession.cs
@@ -0,0 +1,12 @@
+﻿using MimicaApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MimicaApp.Storage {
+    public class GameSession {
+        public static Game Game { get; set; }
+
+        public static short CurrentRound { get; set; }
+    }
+}
