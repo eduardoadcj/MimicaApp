5543415c765176d7ecf84984cd7e41a7ed8c318e
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-08-03 11:44:21 -0300

    Construção da interface da tela inicial

--- MimicaApp/MimicaApp/App.xaml.cs
+++ MimicaApp/MimicaApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MimicaApp.View;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,7 +8,7 @@ namespace MimicaApp {
         public App() {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new HomePage();
         }
 
         protected override void OnStart() {
