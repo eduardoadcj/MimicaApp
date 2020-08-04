5b358ee8c8d0e7aaf2d76260248570c1a169dcb9
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-08-04 14:29:24 -0300

    Implementação de converter

---
+++ MimicaApp/MimicaApp/View/Util/Converter/LabelPointsConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MimicaApp.View.Util.Converter {
+    public class LabelPointsConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            return value + " points";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            return short.Parse((value as string).Replace(" points", ""));
+        }
+    }
+}
