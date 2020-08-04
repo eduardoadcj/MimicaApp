9cb73ca55926490331ec967363c2b4ce34da3588
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2020-08-04 14:07:53 -0300

    Criação da tela resultado

--- MimicaApp/MimicaApp/Model/Game.cs
+++ MimicaApp/MimicaApp/Model/Game.cs
@@ -11,5 +11,14 @@ namespace MimicaApp.Model {
         public string Mode { get; set; }
         public short WordTime { get; set; }
         public short Rounds { get; set; }
+
+        public Team GetWinner() {
+            return TeamA.Score >= TeamB.Score ? TeamA : TeamB;
+        }
+
+        public Team GetLoser() {
+            return TeamA.Score <= TeamB.Score ? TeamA : TeamB;
+        }
+
     }
 }
--- MimicaApp/MimicaApp/ViewModel/ResultViewModel.cs
+++ MimicaApp/MimicaApp/ViewModel/ResultViewModel.cs
@@ -1,8 +1,35 @@
-﻿using System;
+﻿using MimicaApp.Model;
+using MimicaApp.Storage;
+using MimicaApp.View;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using Xamarin.Forms;
 
 namespace MimicaApp.ViewModel {
-    class ResultViewModel {
+    public class ResultViewModel : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string nameProperty) {
+            if(PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(nameProperty));
+            }
+        }
+
+        public Team WinnerTeam { get; set; }
+        public Team LoserTeam { get; set; }
+
+        public Command PlayAgain { get; set; }
+
+        public ResultViewModel() {
+            PlayAgain = new Command(OnPlayAgain);
+            WinnerTeam = GameSession.Game.GetWinner();
+            LoserTeam = GameSession.Game.GetLoser();
+        }
+
+        private void OnPlayAgain() {
+            App.Current.MainPage = new HomePage();
+        }
+
     }
 }
